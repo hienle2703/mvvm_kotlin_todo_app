f6ec483ae33d8ca09fae0d0657e3ebc4f90f7be5
hienle2703 <levutoanhien2703@gmail.com>
2022-05-09 14:59:45 +0700

    - Add UI Sign in 100%

--- app/src/main/java/com/codinginflow/mvvmtodo/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/auth/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.codinginflow.mvvmtodo.ui.auth
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.codinginflow.mvvmtodo.R
@@ -7,6 +8,8 @@ import com.codinginflow.mvvmtodo.R
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        window.statusBarColor = Color.WHITE
         setContentView(R.layout.activity_login)
     }
 }
