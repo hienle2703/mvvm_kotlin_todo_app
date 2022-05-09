24ea7698cc1093ff868a664a305b92831b425773
hienle2703 <levutoanhien2703@gmail.com>
2022-05-09 23:56:16 +0700

    - Set up firebase - Add navigation for auth stacks - Add function sign up (30%)

--- app/src/main/java/com/codinginflow/mvvmtodo/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/auth/LoginActivity.kt
@@ -1,15 +1,34 @@
 package com.codinginflow.mvvmtodo.ui.auth
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.codinginflow.mvvmtodo.R
+import com.codinginflow.mvvmtodo.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         window.statusBarColor = Color.WHITE
-        setContentView(R.layout.activity_login)
+
+
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.apply {
+            btnGoSignup.setOnClickListener {
+
+                println("HELLO")
+                val intent = Intent(
+                    this@LoginActivity, RegistrationActivity::class.java
+                )
+                startActivity(intent)
+
+            }
+        }
+
+
     }
 }
--- app/src/main/java/com/codinginflow/mvvmtodo/ui/auth/RegistrationActivity.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/auth/RegistrationActivity.kt
@@ -1,13 +1,66 @@
 package com.codinginflow.mvvmtodo.ui.auth
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.codinginflow.mvvmtodo.R
+import com.codinginflow.mvvmtodo.databinding.ActivityRegistrationBinding
+import com.codinginflow.mvvmtodo.ui.HomeActivity
+import com.google.firebase.auth.FirebaseAuth
 
 public class RegistrationActivity : AppCompatActivity() {
 
+    private lateinit var email: String
+    private lateinit var password: String
+    private lateinit var confirmPassword: String
+
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registration)
+
+        supportActionBar?.hide()
+        window.statusBarColor = Color.WHITE
+
+        mAuth = FirebaseAuth.getInstance()
+
+        val binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            btnGoBack.setOnClickListener {
+                finish()
+            }
+
+            btnSignUp.setOnClickListener {
+                email = inputEmail.text.toString().trim()
+                password = inputPassword.text.toString().trim()
+                confirmPassword = inputConfirmPassword.text.toString().trim()
+
+                val intent = Intent(this@RegistrationActivity, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+
+                if (email.isEmpty() && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    inputEmail.error = "Email can not be empty!"
+                    return@setOnClickListener
+                }
+                if (password.isEmpty()) {
+                    inputPassword.error = "Password can not be empty!"
+                    return@setOnClickListener
+                }
+                if (password != confirmPassword) {
+                    inputConfirmPassword.error = "Confirm password not matched!"
+                    return@setOnClickListener
+                }
+
+//                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+//                    val intent = Intent(this@RegistrationActivity, HomeActivity::class.java)
+//                    startActivity(intent)
+//                    finish()
+//                }
+
+            }
+        }
     }
 }
